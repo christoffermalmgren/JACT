588944636e103ac62d292c3de07d0ec050a00cbc
Christoffer Malmgren <christoffer.malmgren@chasacademy.se>
2019-03-20 21:16:45 +0100

    deleted server side

--- src/server/models/pics.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const Schema = mongoose.Schema
-
-const picsSchema = new Schema({
-    pics: String
-})
-
-module.exports = mongoose.model('pic', picsSchema, 'pics')
